e0eff3881f81ee666faba8337cf91cc2b5d85fef
Greg Brener <gbrener@gmail.com>
2015-12-25 23:13:25 -0600

    Add to pythonrc

--- .pythonrc.py
+++ .pythonrc.py
@@ -5,24 +5,28 @@ import re
 import inspect
 import numpy as np
 import pandas as pd
-
 from collections import namedtuple, defaultdict, OrderedDict, Counter, deque
 from datetime import datetime, date, time, timedelta
 from dateutil.parser import parse as parse_date
 
-### IPython config
+#### IPython config
+ipython_ver = None
 try:
-    # Create IPython config directory if not exists
-    ipython_dir = os.path.join(os.getenv('HOME'), 'Desktop', 'ipython')
-    if not os.path.isdir(ipython_dir):
-        os.mkdir(ipython_dir)
-    c.BaseIPythonApplication.ipython_dir = ipython_dir
-
     c = get_config()
+    c.InteractiveShellApp.exec_PYTHONSTARTUP = False
     c.TerminalIPythonApp.display_banner = False
     c.InteractiveShell.display_page = True # export PAGER=cat
     c.TerminalInteractiveShell.confirm_exit = False
     c.TerminalInteractiveShell.editor = os.getenv('VISUAL', os.getenv('EDITOR', 'nano'))
+
+    import IPython
+    ipython_ver = IPython.__version__
 except NameError:
-    this_file = re.split(r'#+ IPython', open(__file__).read(), 1)[0]
-    print(this_file.rstrip()+'\n')
+    pass
+
+this_file = re.split(r'#+ IPython', open(__file__).read(), 1)[0]
+ver = sys.version_info
+print(this_file.rstrip()+'\n')
+print('Python  v{0}.{1}.{2}'.format(ver.major, ver.minor, ver.micro))
+if ipython_ver is not None:
+    print('IPython v{0}'.format(ipython_ver))
